547b0b2862ee781a2720c600a2f8cd5008c4dde5
quad400 <adedijiabdulquadri@gmail.com>
2024-02-05 03:52:10 +0100

    db updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,16 @@
-const {default: mongoose} = require("mongoose")
+const { default: mongoose } = require("mongoose");
 
-const dbConnect = ()=>{
-    try {
-        mongoose.connect(process.env.MONGO_URI)
-        console.log("Database connected successfully")
-    } catch (error) {
-        console.log("Unable to connect database", error)
+const dbConnect = () => {
+  try {
+    if (process.env.NODE_ENV_MODE === "production") {
+      mongoose.connect(process.env.MONGO_URI);
+    } else {
+      mongoose.connect(process.env.MONGO_URI_DEV);
     }
-}
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Unable to connect database", error);
+  }
+};
 
 module.exports = dbConnect;
